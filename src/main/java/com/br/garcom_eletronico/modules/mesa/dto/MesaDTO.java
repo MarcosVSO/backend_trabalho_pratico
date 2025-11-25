e63d96cda3ce7c8589bb5530b06ce8c3f065c814
Marcos Vinicius Souza Oliveira <marcosv.soliver@gmail.com>
2025-11-25 15:48:24 -0300

    Finalizada implementações da classe mesa

---
+++ src/main/java/com/br/garcom_eletronico/modules/mesa/dto/MesaDTO.java
@@ -0,0 +1,10 @@
+package com.br.garcom_eletronico.modules.mesa.dto;
+
+import lombok.Data;
+
+@Data
+public class MesaDTO {
+    private Integer numero;
+    private Boolean disponivel;
+}
+
---
+++ src/main/java/com/br/garcom_eletronico/modules/mesa/repository/MesaRepository.java
@@ -0,0 +1,8 @@
+package com.br.garcom_eletronico.modules.mesa.repository;
+
+import com.br.garcom_eletronico.modules.mesa.Mesa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MesaRepository extends JpaRepository<Mesa, Integer> {
+}
+
