ae94de6e44f1d7b24fc0b09a04c62314fef36b6a
Marcos Vinicius Souza Oliveira <marcosv.soliver@gmail.com>
2025-11-25 17:25:28 -0300

    Implementaçõe garçom

---
+++ src/main/java/com/br/garcom_eletronico/modules/garcom/dto/GarcomDTO.java
@@ -0,0 +1,10 @@
+package com.br.garcom_eletronico.modules.garcom.dto;
+
+import lombok.Data;
+
+@Data
+public class GarcomDTO {
+    private Long id;
+    private String nome;
+}
+
---
+++ src/main/java/com/br/garcom_eletronico/modules/garcom/repository/GarcomRepository.java
@@ -0,0 +1,8 @@
+package com.br.garcom_eletronico.modules.garcom.repository;
+
+import com.br.garcom_eletronico.modules.garcom.Garcom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GarcomRepository extends JpaRepository<Garcom, Long> {
+}
+
