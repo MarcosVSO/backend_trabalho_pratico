d49971eecf944364e47f466167ca97ab1ec4ffd1
Marcos Vinicius Souza Oliveira <marcosv.soliver@gmail.com>
2025-12-10 12:47:12 -0300

    Ajusta native query

--- src/main/java/com/br/garcom_eletronico/modules/cliente/repository/ClienteRepository.java
+++ src/main/java/com/br/garcom_eletronico/modules/cliente/repository/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.br.garcom_eletronico.modules.cliente.repository;
 import com.br.garcom_eletronico.modules.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,4 +15,15 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     
     @Query(value = "SELECT * FROM cliente ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Optional<Cliente> findRandomCliente();
+
+    @Query(
+            value = """
+                SELECT *
+                FROM cliente
+                WHERE EXTRACT(DAY FROM data_nascimento) = :dia
+                  AND EXTRACT(MONTH FROM data_nascimento) = :mes
+                """,
+            nativeQuery = true
+    )
+    List<Cliente> findAniversariantes(@Param("dia") int dia, @Param("mes") int mes);
 }
